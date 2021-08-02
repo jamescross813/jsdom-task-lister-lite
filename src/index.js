7c60a886c9d9d40aaf1ae35370f7e485b0043742
jamescross813 <jamescross@TrashPanda.localdomain>
2021-08-01 22:24:41 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const task = document.getElementById("list")
-  const taskFrom = document.getElementById("create-task-form")
+  const task = document.getElementById("list");
+  const taskFrom = document.getElementById("create-task-form");
   
   function handleSubmit(e){
     event.preventDefault();
-    const input = document.getElementById("new-task-description")
-    const li = document.createElement('li')
+    const input = document.getElementById("new-task-description");
+    const li = document.createElement('li');
    
-    li.innerText = e.target[0].value
-    list.appendChild(li)
+    li.innerText = e.target[0].value;
+    list.appendChild(li);
   }
 
-  taskFrom.addEventListener('submit', handleSubmit)
+  taskFrom.addEventListener('submit', handleSubmit);
 });
